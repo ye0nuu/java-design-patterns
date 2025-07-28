de8092f2b597fe76ede3c0240e50e0c116fc4124
ye0nuu <dusdn1919@naver.com>
2025-07-28 15:37:58 +0900

    퍼사드 메일 수정

--- src/main/java/_02_structural_patterns/_10_facade/Client.java
+++ src/main/java/_02_structural_patterns/_10_facade/Client.java
@@ -3,19 +3,26 @@ package _02_structural_patterns._10_facade;
 public class Client {
     public static void main(String[] args) {
 
-        EmailSettings emailSettings = new EmailSettings();
-        emailSettings.setHost("127.0.0.1");
+        try {
+            EmailSettings emailSettings = new EmailSettings();
+            emailSettings.setHost("127.0.0.1");
 
-        EmailSender emailSender = new EmailSender(emailSettings);
+            EmailSender emailSender = new EmailSender(emailSettings);
 
-        EmailMessage emailMessage = new EmailMessage();
-        emailMessage.setFrom("yeonwoo");
-        emailMessage.setTo("whiteship");
-        emailMessage.setCc("일남");
-        emailMessage.setSubject("오징어게임");
-        emailMessaeg.setText("...");
+            EmailMessage emailMessage = new EmailMessage();
+            emailMessage.setFrom("noreply@facadepattern.com");
+            emailMessage.setTo("client@yourproject.com");
+            emailMessage.setCc("backend@yourproject.com");
+            emailMessage.setSubject("요청이 퍼사드에 의해 처리");
+            emailMessage.setText("복잡한 서브 시스템은 몰라도 됨");
 
-        emailSender.sendEmail(emailMessage);
+            // 이 한 줄만 호출하면 메일 전송
+            emailSender.sendEmail(emailMessage);
+
+        } catch (Exception e) {
+            System.out.println("이메일 전송 중 예외 발생 (실습 환경에서는 무시 가능): " + e.getMessage());
+            e.printStackTrace();
+        }
 
     }
 }
